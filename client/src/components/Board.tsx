025f38566f2c9e7c801e9cf7a4e7aa2e17b33998
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-01-29 03:09:50 +0000

    up

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,7 +1,13 @@
 export function Board() {
   return (
     <div className="chessboard">
-      <div className="square"></div>
+      {[...Array(8).keys()].map((row) => (
+        <div className="chessboard-row" key={String(row)}>
+          {[...Array(8).keys()].map((col) => (
+            <div className="square" key={String(col)}></div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
