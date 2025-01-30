44fbf37cbcb51c936755850d93dafcc2e23d3939
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-01-30 21:27:29 +0000

    added vite client/.env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,21 +7,15 @@ import { NotFound } from './pages/NotFound';
 
 import './App.css';
 
+const AppSubdomain = '/' + import.meta.env.VITE_APP_SUB_DOMAIN;
+
 export default function App() {
   return (
     <Routes>
-      <Route
-        path={'/' + import.meta.env.VITE_APP_SUB_DOMAIN}
-        element={<Header />}>
+      <Route path={AppSubdomain} element={<Header />}>
         <Route index element={<Game />} />
-        <Route
-          path={'/' + import.meta.env.VITE_APP_SUB_DOMAIN + 'about'}
-          element={<About />}
-        />
-        <Route
-          path={'/' + import.meta.env.VITE_APP_SUB_DOMAIN + 'credits'}
-          element={<Credits />}
-        />
+        <Route path={AppSubdomain + 'about'} element={<About />} />
+        <Route path={AppSubdomain + 'credits'} element={<Credits />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
