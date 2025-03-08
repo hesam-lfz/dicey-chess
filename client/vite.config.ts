9c732aa3af5eaa03e10a54036f12c06d83e44852
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-03-08 06:48:48 +0000

    up

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dicey-chess-web/',
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
