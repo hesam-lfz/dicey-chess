517bc23dff4587415fc26dc8ae5094b8b3f6fa24
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-05-02 03:43:58 +0000

    up

--- app/DiceyChess/app/(tabs)/settings.tsx
+++ app/DiceyChess/app/(tabs)/settings.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { vw, vh } from 'react-native-css-vh-vw';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -6,32 +7,58 @@ import { ToggleSwitch } from '@/components/ui/ToggleSwitch';
 
 export default function TabTwoScreen() {
   return (
-    <>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title" style={styles.text}>
-          Settings
-        </ThemedText>
+    <ThemedView
+      style={{
+        ...styles.mainPanel,
+        ...styles.paddedMainPanel,
+        ...styles.flex,
+        ...styles.flexCol,
+        ...styles.flexAlignCenter,
+      }}>
+      <ThemedText type="title" style={styles.text}>
+        Settings
+      </ThemedText>
+      <ThemedView
+        style={{
+          ...styles.flex,
+          ...styles.flexCol,
+          ...styles.flexAlignCenter,
+        }}>
+        <ThemedText style={styles.text}>Blah</ThemedText>
+        <ToggleSwitch
+          label="Play vs. AI"
+          initChecked={false}
+          containerOnChange={() => {}}
+        />
       </ThemedView>
-      <ToggleSwitch
-        label="Play vs. AI"
-        initChecked={false}
-        containerOnChange={() => {}}
-      />
-    </>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
+  mainPanel: {
+    display: 'flex',
     justifyContent: 'center',
-    gap: 8,
+    alignItems: 'flex-start',
+    alignSelf: 'center',
+    height: vh(100),
+    width: vw(100),
+  },
+  paddedMainPanel: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: vw(10),
+    paddingRight: vw(10),
+  },
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  flexCol: {
+    flexDirection: 'column',
+  },
+  flexAlignCenter: {
+    alignItems: 'center',
   },
   text: {
     fontFamily: 'Sono',
